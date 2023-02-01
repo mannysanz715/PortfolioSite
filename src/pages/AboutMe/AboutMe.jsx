5f4c878e98e710c371cc3179a51a20b3ce61b8cc
MannySanz <mannysanz715@gmail.com>
2023-02-01 15:21:59 -0800

    Add Paragraph to about me

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -2,6 +2,11 @@ const AboutMe = (props) => {
   return (
     <>
       <h1>About Me</h1>
+      <div>
+        <p className="about-me-paragraph">
+          I have always been a fast learner and have a passion for knowledge, whether it was picking up the guitar at the age of 6, building computers in highschool, or learning software development. The one thing that all of my hobbies and interests have in common is the need to invest time and dedication to become proficient in them. Anybody can strum a guitar and say they know how to play it, or write "Hello World" in the console and become a programmer, but I dedicate time and effort into each of my passions to improve on myself on a daily basis. With my eagerness to learn, and patience of a driving instructor from Los Angeles, I've been able to learn and acomplish everything I've set my mind to!
+        </p>
+      </div>
     </>
 )
 }
