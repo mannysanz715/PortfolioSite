24fff7bd6b5ced7478bb3c29540f8cb0cf886b46
mannysanz715 <mannysanz715@gmail.com>
2023-02-01 19:29:48 -0800

    Change ! and technologies

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -6,11 +6,11 @@ const AboutMe = (props) => {
       <img className="profile-image animate__zoomIn animate__delay-.5s animate__animated" src="/mannypic.png" alt="" width='300px' />
       <h2>About Me</h2>
         <p className="about-me-paragraph">
-          Hi!! My name is Manny and I am a Software Developer. I got into programming when I was in High School, and have stuck with it since then. Building out projects and getting to work with the people around me on projects is why I became a developer in the first place. I also really love music, and picking up new skill come to me easily. I taught myself the guitar when I was 6, played violin in Middle School, and just recently picked up the piano and saxophone. I wonder what cool thing I will pick up next!?!? 
+          Hi! My name is Manny and I am a Software Developer. I got into programming when I was in High School, and have stuck with it since then. Building out projects and getting to work with the people around me on projects is why I became a developer in the first place. I also really love music, and picking up new skill come to me easily. I taught myself the guitar when I was 6, played violin in Middle School, and just recently picked up the piano and saxophone. I wonder what cool thing I will pick up next!?!? 
         </p>
         <br/>
         <br/>
-        <h2>Technologies I know:</h2>
+        <h2>My ToolSet:</h2>
         <div className="tech-used-container">
           <img src="/react.gif" alt="reactIcon"></img>
           <img src="/express.png" alt="expressIcon"></img>
