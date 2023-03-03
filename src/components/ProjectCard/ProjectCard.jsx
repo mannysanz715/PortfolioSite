9c670c7c00f86870a86384cfbde2e2b70b5f8a9c
MannySanz <mannysanz715@gmail.com>
2023-03-03 11:04:54 -0800

    Projects Update

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -10,19 +10,19 @@ const ProjectCard = ({project, index}) => {
         <div className='title-image'>
           <h3>{project.title}</h3>
           <img className="project-img" src={project.img} alt="Project pic" />
+          <div className='card-buttons'>
+        <a href={project.link} target="_blank" rel="noreferrer"><button className="projects-buttons">Live Demo</button></a>
+        <a href={project.gitLink} target="_blank" rel="noreferrer"><button className="projects-buttons">GitHub</button></a>
+        </div>
         </div>
         <div className='card-desc'>
-          <p>{project.desc}</p>
           <h3>Technologies:</h3>
         <ul className='tech-container'>
         {project.techUsed.map( tech =>
           <li key={tech} >{tech}</li>
           )}
         </ul>
-        <div className='card-buttons'>
-        <a href={project.link} target="_blank" rel="noreferrer"><button className="projects-buttons">Live Demo</button></a>
-        <a href={project.gitLink} target="_blank" rel="noreferrer"><button className="projects-buttons">GitHub</button></a>
-        </div>
+          <p>{project.desc}</p>
         </div>
       </div>
 
