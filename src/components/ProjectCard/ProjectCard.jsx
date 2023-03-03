81373cb2f74709f04d2d691f1175cff297e6c75a
MannySanz <mannysanz715@gmail.com>
2023-03-03 11:26:12 -0800

    Change Responsiveness and button styles

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -11,18 +11,18 @@ const ProjectCard = ({project, index}) => {
           <h3>{project.title}</h3>
           <img className="project-img" src={project.img} alt="Project pic" />
           <div className='card-buttons'>
-        <a href={project.link} target="_blank" rel="noreferrer"><button className="projects-buttons">Live Demo</button></a>
-        <a href={project.gitLink} target="_blank" rel="noreferrer"><button className="projects-buttons">GitHub</button></a>
-        </div>
+            <a href={project.link} target="_blank" rel="noreferrer"><button className="projects-buttons">Live Demo</button></a>
+            <a href={project.gitLink} target="_blank" rel="noreferrer"><button className="projects-buttons">GitHub</button></a>
+          </div>
         </div>
         <div className='card-desc'>
           <h3>Technologies:</h3>
-        <ul className='tech-container'>
-        {project.techUsed.map( tech =>
-          <li key={tech} >{tech}</li>
-          )}
-        </ul>
-          <p>{project.desc}</p>
+          <ul className='tech-container'>
+            {project.techUsed.map( tech =>
+            <li key={tech} >{tech}</li>
+            )}
+          </ul>
+            <p>{project.desc}</p>
         </div>
       </div>
 
