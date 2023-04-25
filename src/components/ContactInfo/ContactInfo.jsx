742e5a3318effbc2fa7fea08a9047a34728daf41
MannySanz <mannysanz715@gmail.com>
2023-04-24 19:54:33 -0700

    Add contact images and buttons

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -7,13 +7,13 @@ const ContactInfo = () => {
       </h1>
       <ul className='contact-list'>
       <a target="_blank" href='https://www.linkedin.com/in/manuel-sanchez-leandro/'><li className="linkedIn list-item">
-          LinkedIn : https://www.linkedin.com/in/manuel-sanchez-leandro/
+          LinkedIn <img src='/linkedin.png' alt='linkedIn-img'/>
         </li></a>
         <a href='https://github.com/mannysanz715' target='_blank'><li className="github list-item">
-          GitHub:https://github.com/mannysanz715
+          GitHub <img src='/github.png' alt='github-img'/>
         </li></a>
-        <a href="https://drive.google.com/file/d/1AMKMILOEmWyopWbh-0aeYdZEJS4JyOmh/view?usp=share_link" target='_blank'><li className="resume list-item">Resume</li></a>
-        <a href="mailto:mannysleandro@gmail.com"><li className="email list-item">Email Me : mannysleandro@gmail.com</li></a>
+        <a href="https://drive.google.com/file/d/1AMKMILOEmWyopWbh-0aeYdZEJS4JyOmh/view?usp=share_link" target='_blank'><li className="resume list-item">Resume <img src='/resume.png' alt='email-img'/></li></a>
+        <a href="mailto:mannysleandro@gmail.com"><li className="email list-item">Email Me <img src='/gmail.png' alt='email-img'/></li></a>
       </ul>
     </div>
 )
